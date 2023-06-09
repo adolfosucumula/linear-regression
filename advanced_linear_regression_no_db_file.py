19e71c047ae5e803c2d579d6bb0a11d50309bdc3
adolfosucumula <rocketmc2009@gmail.com>
2023-06-09 15:36:27 +0100

    advanced linear regression

---
+++ advanced_linear_regression_no_db_file.py
@@ -0,0 +1,17 @@
+import numpy as np
+import statsmodels.api as sm
+
+
+X= np.array([[0,1],[2,3],[3,5],[4,8],[5,10],[6,12],[7,14],[8,16],[9,18],[10,20]])
+y = np.array([2,4,5,6,8,10,11,12,14,16])
+
+X = sm.add_constant(X)
+
+# Create model and fit it
+model = sm.OLS(y, X)
+
+results = model.fit()
+
+print(f"Coefficient of determination: {results.rsquared}")
+print(f"Adjusted coefficient of determination: {results.rsquared_adj}")
+print(f"Regression coefficient: {results.params}")
